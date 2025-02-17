a68d07e4966bba4f3d14e4ee33691fc20b2e02da
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-02-17 20:02:15 +0100

    Adding Metadata to Pages

---
+++ app/auth/register/layout.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+      title: "CashTrackr - Crear Cuenta",
+      description: "CashTrackr - Crear Cuenta"
+};
+
+export default function RegisterLayout({ children }: { children: React.ReactNode }) {
+      return <>{children}</>;
+}
