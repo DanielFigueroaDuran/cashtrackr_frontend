290e9e757150434e7eed7f51c4113ce3f071d0d6
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-02-19 22:07:18 +0100

    adding image with next component

--- app/components/ui/Logo.tsx
+++ app/components/ui/Logo.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image"
 
 const Logo = () => {
       return (
-            <img src="/logo.svg" alt="Logo" width={400} height={100} />
+            <Image
+                  src="/logo.svg"
+                  alt="Logo"
+                  width={0}
+                  height={0}
+                  className="w-full"
+                  priority
+            />
       )
 }
 
