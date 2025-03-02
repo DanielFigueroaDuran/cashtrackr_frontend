b004946e331060f3edc4f86fa7363e9747279301
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-03-02 13:22:18 +0100

    Storing the Token in the State

--- app/components/auth/ConfirmAccoountForm.tsx
+++ app/components/auth/ConfirmAccoountForm.tsx
@@ -1,10 +1,26 @@
 "use client"
 import { PinInput, PinInputField } from "@chakra-ui/pin-input";
+import { useState } from "react";
+import { generateToken } from '../../../../backend/src/utils/token';
 
 const ConfirmAccoountForm = () => {
+      const [token, setToken] = useState('');
+
+      const handleChange = (token: string) => {
+            setToken(token);
+      };
+
+      const handleComplete = () => {
+
+      };
+
       return (
             <div className="flex justify-center gap-5 my-10">
-                  <PinInput>
+                  <PinInput
+                        value={token}
+                        onChange={handleChange}
+                        onComplete={handleComplete}
+                  >
                         <PinInputField className="w-10 h-10 border-gray-300 shadow rounded-lg text-center placeholder-white" />
                         <PinInputField className="w-10 h-10 border-gray-300 shadow rounded-lg text-center placeholder-white" />
                         <PinInputField className="w-10 h-10 border-gray-300 shadow rounded-lg text-center placeholder-white" />
