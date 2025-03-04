2605e4c0f4d0a19ecd02ab29470db81590a1912e
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-03-04 20:28:33 +0100

    Sending the request to the Backend

--- actions/autenticate-user-action.ts
+++ actions/autenticate-user-action.ts
@@ -21,6 +21,23 @@ export const authenticate = async (prevState: ActionStatetype, formData: FormDat
             };
       };
 
+      const url = `${process.env.API_URL}/api/auth/login`;
+      const req = await fetch(url, {
+            method: 'POST',
+            headers: {
+                  'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                  password: auth.data.password,
+                  email: auth.data.email
+            })
+      });
+
+      const json = await req.json();
+
+      console.log('req.ok', req.ok);
+      console.log('json', json);
+
       return {
             errors: []
       };
