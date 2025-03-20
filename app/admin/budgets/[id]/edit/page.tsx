1494165fd51e710726cb1ed92a19469ac3c7fd44
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-03-20 22:01:10 +0100

    Validating the Budget with a ZOD schema and edit page

--- app/admin/budgets/[id]/edit/page.tsx
+++ app/admin/budgets/[id]/edit/page.tsx
@@ -1,4 +1,7 @@
+import { BudgetAPIResponseSchema } from "@/src/shemas";
 import { cookies } from "next/headers";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
 const getBudget = async (budgetId: string) => {
       const token = (await cookies()).get('CASHTRACKR_TOKEN')?.value;
@@ -13,10 +16,11 @@ const getBudget = async (budgetId: string) => {
       //console.log(req.ok);
 
       if (!req.ok) {
-
+            notFound();
       };
 
-      return json;
+      const budget = BudgetAPIResponseSchema.parse(json)
+      return budget;
 };
 
 const EditBudgetPage = async ({ params }: { params: { id: string } }) => {
@@ -24,7 +28,27 @@ const EditBudgetPage = async ({ params }: { params: { id: string } }) => {
       const budget = await getBudget(id);
 
       return (
-            <div>EditBudgetPage</div>
+            <>
+                  <div className='flex flex-col-reverse md:flex-row md:justify-between items-center'>
+                        <div className='w-full md:w-auto'>
+                              <h1 className='font-black text-4xl text-purple-950 my-5'>
+                                    Editar Presupuesto: {budget.name}
+                              </h1>
+                              <p className="text-xl font-bold">Llena el formulario y crea un nuevo {''}
+                                    <span className="text-amber-500">presupuesto</span>
+                              </p>
+                        </div>
+                        <Link
+                              href={'/admin'}
+                              className='bg-amber-500 p-2 rounded-lg text-white font-bold w-full md:w-auto text-center'
+                        >
+                              Volver
+                        </Link>
+                  </div>
+                  <div className='p-10 mt-10  shadow-lg border '>
+
+                  </div>
+            </>
       )
 }
 
