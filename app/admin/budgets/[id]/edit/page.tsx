7a8e1f30dd4199413f917b07b53869d1f6565274
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-03-20 19:40:11 +0100

    Consult the API to get the quote

--- app/admin/budgets/[id]/edit/page.tsx
+++ app/admin/budgets/[id]/edit/page.tsx
@@ -1,8 +1,22 @@
+import { cookies } from "next/headers";
 
+const getBudget = async (budgetId: string) => {
+      const token = (await cookies()).get('CASHTRACKR_TOKEN')?.value;
+      const url = `${process.env.API_URL}/api/budgets/${budgetId}`;
+
+      const req = await fetch(url, {
+            headers: {
+                  'Authorization': `Bearer ${token}`
+            }
+      });
+      const json = await req.json();
+      console.log(req.ok);
+};
 
 const EditBudgetPage = async ({ params }: { params: { id: string } }) => {
       const { id } = await params;
-      console.log(id);
+      await getBudget(id);
+
       return (
             <div>EditBudgetPage</div>
       )
