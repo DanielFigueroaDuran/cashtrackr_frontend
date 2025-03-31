fd4be1c3f7af7a94fc3149481553642f4ccd2e40
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-03-31 22:36:05 +0200

    Querying the API to get the quote (API Route)

--- app/admin/api/budget/[budgetId]/expenses/[expenseId]/route.ts
+++ app/admin/api/budget/[budgetId]/expenses/[expenseId]/route.ts
@@ -1,6 +1,22 @@
 import { verifySession } from "@/src/auth/dal";
+import { cookies } from "next/headers";
 
-export const GET = async () => {
+export const GET = async (request: Request, { params }: { params: { budgetId: string, expenseId: string } }) => {
       await verifySession();
-      return Response.json('Hola Mundo');
+
+      const token = (await cookies()).get('CASHTRACKR_TOKEN')?.value;
+      const url = `${process.env.API_URL}/api/budgets/${params.budgetId}/expenses/${params.expenseId}`;
+      const req = await fetch(url, {
+            headers: {
+                  'Authorization': `Bearer ${token}`
+            }
+      });
+
+      const json = await req.json();
+
+      if (!req.ok) {
+            return Response.json(json.error, { status: 403 });
+      };
+
+      return Response.json(json);
 };
