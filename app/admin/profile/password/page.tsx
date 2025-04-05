70ac89634026a9fd40f664a7f84a0bebf33a5260
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-04-05 19:09:04 +0200

    Creating Layout and Routing for User Preferences

---
+++ app/admin/profile/password/page.tsx
@@ -0,0 +1,15 @@
+
+
+const ChangePasswordPage = async () => {
+      return (
+            <>
+                  <h1 className="font-black text-4xl text-purple-950 my-5">Cambiar Password</h1>
+                  <p className="text-xl font-bold">Aquí puedes cambiar tu {''}
+                        <span className="text-amber-500">password</span>
+                  </p>
+            </>
+      )
+}
+
+
+export default ChangePasswordPage;
